91d9013ea17b0cfccbedd45c82182a354fcf0c06
Sogol Sheydaei <76821858+sogolsheydaei@users.noreply.github.com>
2025-04-05 11:46:26 +0200

    The IN Operator.sql

--- The IN Operator.sql
+++ The IN Operator.sql
@@ -2,4 +2,5 @@ SELECT *
 FROM customers
 -- WHERE state = 'VA' OR state = 'GA' OR state = 'FL'
 -- WHERE state IN ('VA', 'GA', 'FL')
-WHERE state NOT IN ('VA', 'GA', 'FL')
+WHERE state NOT IN ('VA', 'GA', 'FL') 
+-- if we were searching in numbers we do not need ''  anymore.
