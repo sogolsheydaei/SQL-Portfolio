e5c8aaa8132f2a69345cb7d70b4f2275e7431df0
Sogol Sheydaei <76821858+sogolsheydaei@users.noreply.github.com>
2025-04-06 20:27:14 +0200

    Outer Join Between Multiple Tables

---
+++ Outer Join Between Multiple Tables.sql
@@ -0,0 +1,13 @@
+SELECT 
+	c.customer_id,
+    c.first_name,
+    o.order_id,
+    sh.name AS shipper
+FROM customers c
+LEFT JOIN orders o
+	ON c.customer_id = o.customer_id 
+LEFT JOIN shippers sh
+    ON o.shipper_id = sh.shipper_id
+
+ORDER BY C.customer_id
+
