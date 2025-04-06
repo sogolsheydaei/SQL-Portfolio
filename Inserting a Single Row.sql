3aab8c9ea907aeace38a490f4d59af39feeb5cb7
Sogol Sheydaei <76821858+sogolsheydaei@users.noreply.github.com>
2025-04-06 22:23:27 +0200

    Inserting a Single Row

---
+++ Inserting a Single Row.sql
@@ -0,0 +1,30 @@
+/*
+INSERT INTO customers 
+VALUES (default,
+		'Sogol',
+        'Sheydaei',
+        NULL,
+        NULL,
+        'address',
+        'Hamburg',
+        'Ha',
+        default
+        )
+ */       
+INSERT INTO customers (
+	first_name,
+    last_name,
+    birth_date,
+    address,
+    city,
+    state
+)
+VALUES (
+		'Sogol',
+        'Sheydaei',
+        '1990-01-01',
+        'address',
+        'Hamburg',
+        'Ha'
+        )
+        
